d40b3ecfb3d5cf5d3434518e5c45e3e0a9622b92
Маруся <masha.rakitina@gmail.com>
2023-11-19 13:19:45 +0300

    some edit

--- 0_SQL_BootCamp/Day_02/ex02/day02_ex02.sql
+++ 0_SQL_BootCamp/Day_02/ex02/day02_ex02.sql
@@ -7,3 +7,14 @@ FROM (SELECT *
          FULL JOIN pizzeria piz on piz.id = pv.pizzeria_id
          FULL JOIN person per ON pv.person_id = per.id
 ORDER BY person_name, visit_date, pizzeria_name;
+
+-- select
+--     coalesce(p.name, '-') as person_name,
+--     pv.visit_date,
+--     coalesce(pz.name, '-') as pizzeria_name
+-- from pizzeria pz
+--          full join (select * from person_visits
+--                     where visit_date between '2022-01-01' and '2022-01-03') pv
+--                    on pz.id = pv.pizzeria_id
+--          full join person p on pv.person_id = p.id
+-- order by 1,2,3
